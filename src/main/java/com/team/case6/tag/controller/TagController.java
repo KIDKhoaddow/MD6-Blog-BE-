5059f4d935ce4b83f365c84f700c8ae1e7e6ca8b
KIDKhoaddow <phamdangkhoa.k.i.d.6868@gmail.com>
2022-10-14 07:50:59 +0700

    update request tag

--- src/main/java/com/team/case6/tag/controller/TagController.java
+++ src/main/java/com/team/case6/tag/controller/TagController.java
@@ -3,6 +3,7 @@ package com.team.case6.tag.controller;
 import com.team.case6.blog.model.entity.Blog;
 import com.team.case6.blog.service.blog.IBlogService;
 import com.team.case6.blog.service.blogStautus.IBlogStatusService;
+import com.team.case6.category.model.Category;
 import com.team.case6.category.service.ICategoryService;
 import com.team.case6.comment.service.ICommentService;
 import com.team.case6.core.service.userInfo.IUserInfoService;
@@ -39,9 +40,16 @@ public class TagController {
     @Autowired
     private TagMapperImpl tagMapper;
 
-    @GetMapping("/{idBlog}")
+    @GetMapping("/blog/{idBlog}")
     public ResponseEntity<Set<TagDTO>> getListTagByBlog(@PathVariable Long idBlog) {
         Blog blog = blogService.findById(idBlog).get();
         return new ResponseEntity<>(tagMapper.toDto(tagService.findAllByBlog(blog)), HttpStatus.OK);
     }
+
+    @GetMapping("/category/{idCategory}")
+    public ResponseEntity<Set<TagDTO>> getListTagByCategory(@PathVariable Long idCategory) {
+        Category category = categorySV.findById(idCategory).get();
+        return new ResponseEntity<>(tagMapper.toDto(tagService.findAllByCategory(category)), HttpStatus.OK);
+    }
+
 }
